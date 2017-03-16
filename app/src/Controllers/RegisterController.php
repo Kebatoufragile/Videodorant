d517a81f6acc7ecac93f1d92f2e117259ae98a6b
giovanne3u <giovannelli.alexis@gmail.com>
2017-03-17 00:37:57 +0100

    [TODO] debut upload profil pic

--- app/src/Controllers/RegisterController.php
+++ app/src/Controllers/RegisterController.php
@@ -10,6 +10,12 @@ use Cartalyst\Sentinel\Users\UserInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+define('TARGET', "../public/assets/img/profil/");
+define('MAX_SIZE', 2000000); //Taille max en octets du fichier
+define('WIDTH_MAX', 1200);   //Largeur max de l'image en pixels
+define('HEIGHT_MAX', 900);  //Hauteur max de l'image en pixels
+
+
 
 final class RegisterController extends AbstractController{
 
@@ -50,7 +56,89 @@ final class RegisterController extends AbstractController{
 
             $user = User::where('email', 'like', $email)->first();
             $user->username = $identifiant;
-            $user->save();
+
+
+            /***************** Ajout de la photo de profil *****************/
+
+
+                  //Tableaux de données
+            $tabExt = array('jpg','png','jpeg', 'gif'); //Extensions autorisées
+            $infosImg = array();
+
+            //Variables
+            $extension = '';
+            $message = '';
+            $nomImage = '';
+
+            /*************************************************************************
+             * Creation du repertoire cible si inexistant
+             ***********************************************************************/
+            if(!is_dir(TARGET)){
+                if(!mkdir(TARGET, 777)) {
+                    exit('Erreur : le repertoire cible ne peut-être créé ! Vérifiez que vous disposez des droits suffisants pour le faire ou créez le manuellement !');
+                }
+            }
+
+            /*************************************************************************
+             * Script d'upload
+             ************************************************************************/
+            if(!empty($_POST)){
+                //On vérifie si le champ de l'extension est rempli
+                if( !empty($_FILES['picture']['name'])){
+                    //Récupération de l'extension du fichier
+                    $extension = pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION);
+
+                    //On vérifie l'extension du fichier
+                    if(in_array(strtolower($extension), $tabExt)){
+                        //On récupére les dimensions du fichier
+                        $infosImg = getimagesize($_FILES['picture']['tmp_name']);
+
+                        //On vérifie le type de l'image
+                        if($infosImg[2] >= 1 && $infosImg[2] <= 14){
+                            //On vérifie les dimensions et taille de l'image
+                            if(($infosImg[0]<=WIDTH_MAX) && ($infosImg[1]<=HEIGHT_MAX) && (filesize($_FILES['picture']['tmp_name'])<=MAX_SIZE)){
+                                //Parcours du tableau d'erreur
+                                if(isset($_FILES['picture']['error']) && UPLOAD_ERR_OK === $_FILES['picture']['error']){
+                                    //On renomme le fichier
+                                    $nomImage = md5(uniqid()).'.'.$extension;
+
+                                    //Si c'est OK, on test l'upload
+                                    if(move_uploaded_file($_FILES['picture']['tmp_name'], TARGET.$nomImage)){
+                                        //insert database
+                                        //test data
+
+                                        $user->profilPicLink = $nomImage;
+                                        $user->save();
+
+                                    }else{
+                                        //Sinon on affiche une erreur système
+                                        $message = 'A problem has occured during the upload.';
+                                    }
+                                }else{
+                                    $message = 'An internal error prevented the upload.';
+                                }
+                            }else{
+                                //Sinon erreur sur les dimensions et taille de l'image
+                                $message = 'The picture is too big. (Max : 900x1200)';
+                            }
+                        }else{
+                            //Sinon erreur sur le type de l'image
+                            $message = 'The file you want to upload is not a picture.';
+                        }
+                    }else{
+                        //Sinon on affiche une erreur sur l'extension
+                        $message = 'The file extension is uncorrect.';
+                    }
+                }else{
+                    //Sinon on affiche une erreur pour le champ vide
+                    $message = 'Please fill the form.';
+                }
+            }
+        }
+
+
+
+            /***************************************************************/
 
             $this->view['view']->render($response, 'homepage.html.twig', array(
                 'success' => "You have been successfully registered. You can now try log in."
