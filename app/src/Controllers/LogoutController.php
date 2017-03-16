77f2d26a0eec43c694f888d2aae01d4565e85c87
Grenadator <grenadator@gmail.com>
2017-03-16 23:51:45 +0100

    Ajoute LogoutController

---
+++ app/src/Controllers/LogoutController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Cartalyst\Sentinel\Users\UserInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+final class LogoutController extends AbstractController{
+
+    protected $view;
+    protected $logger;
+    private $sentinel;
+
+    public function __construct($view){
+
+        parent::__construct($view);
+        $this->sentinel = new Sentinel();
+        $this->sentinel = $this->sentinel->getSentinel();
+
+    }
+
+    public function logout(Request $request, Response $response, $args){
+
+        if(isset($_SESSION['user'])){
+            $this->sentinel->logout($this->sentinel->getUser());
+            session_destroy();
+
+            $this->view['view']->render($response, 'homepage.html.twig', array(
+                'success' => "You have successfully been logged out."
+            ));
+        }else
+            $this->view['view']->render($response, 'homepage.html.twig', array());
+
+        return $response;
+
+    }
+}
+
+?>
