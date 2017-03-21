a505003e4b27371337335b554ed72825a1d19abe
giovanne3u <giovannelli.alexis@gmail.com>
2017-03-21 22:40:01 +0100

    [TBT] Modify password

--- app/src/Controllers/ProfileController.php
+++ app/src/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Cartalyst\Sentinel\Users\UserInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 Use App\Models\User;
@@ -10,9 +12,12 @@ class ProfileController extends AbstractController{
 
     protected $view;
     protected $logger;
+    private $sentinel;
 
     public function __construct($view){
         parent::__construct($view);
+        $this->sentinel = new Sentinel();
+        $this->sentinel = $this->sentinel->getSentinel();
     }
 
     public function displayProfile(Request $request, Response $response, $args){
@@ -68,7 +73,6 @@ class ProfileController extends AbstractController{
               'error' => 'Vous devez remplir les champs pour modifier vos informations'
             ));
           }
-
         }else{
             $this->view['view']->render($response, 'homepage.html.twig', array(
                 'error' => 'Vous n\'êtes pas connecté.'
@@ -78,13 +82,44 @@ class ProfileController extends AbstractController{
 
 
     public function modifyPassword(Request $request, Response $response, $args){
-//TODO
         if(isset($_SESSION['user'])){
-            $this->view['view']->render($response, 'profile.html.twig', array(
-                'user' => $_SESSION['user'],
-                'success' => 'Votre mot de passe a été modifié.'
-            ));
+          if(isset($_POST['newPassword']) && isset($_POST['actualPassword']) && isset($_POST['newPasswordConfirm'])){
+            if(password_verify(filter_var($_POST['actualPassword'], FILTER_SANITIZE_STRING), $_SESSION['user']->password)){
+              if($_POST['newPassword'] == $_POST['newPasswordConfirm']){
+                $credentials = [
+                  'password' => filter_var($_POST['newPassword'], FILTER_SANITIZE_STRING)
+                ];
 
+                $user = $this->sentinel->findById($_SESSION["user"]->id);
+                if($user = $this->sentinel->update($user, $credentials)){
+                  $this->view['view']->render($response, 'profile.html.twig', array(
+                    'user' => $_SESSION['user'],
+                    'success' => 'Votre mot de passe a été modifié.'
+                  ));
+                }else{
+                  $this->view['view']->render($response, 'profile.html.twig', array(
+                    'user' => $_SESSION['user'],
+                    'error' => "Erreur interne. Votre mot de passe n\'a pas été modifié. Contactez un administrateur."
+                  ));
+                }
+              }else{
+                $this->view['view']->render($response, 'profile.html.twig', array(
+                    'user' => $_SESSION['user'],
+                    'error' => "Mot de passe de confirmation incorrect"
+                ));
+              }
+            }else{
+              $this->view['view']->render($response, 'profile.html.twig', array(
+                'user' => $user,
+                'error' => 'Mot de passe incorrect'
+              ));
+            }
+          }else{
+            $this->view['view']->render($response, 'profile.html.twig', array(
+              'user' => $_SESSION['user'],
+              'error' => "Vous devez remplir les champs pour pouvoir modifier votre mot de passe"
+            ));
+          }
         }else{
             $this->view['view']->render($response, 'homepage.html.twig', array(
                 'error' => 'Vous n\'êtes pas connecté.'
