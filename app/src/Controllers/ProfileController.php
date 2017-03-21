baa835dd4e1695e534cf71868d703c54b7eb6c81
giovanne3u <giovannelli.alexis@gmail.com>
2017-03-21 22:08:57 +0100

    [TBT] modifier les infos utilisateur

--- app/src/Controllers/ProfileController.php
+++ app/src/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+Use App\Models\User;
 
 class ProfileController extends AbstractController{
 
@@ -30,11 +31,43 @@ class ProfileController extends AbstractController{
     public function modifyProfile(Request $request, Response $response, $args){
 
         if(isset($_SESSION['user'])){
-        // TODO
-            $this->view['view']->render($response, 'profile.html.twig', array(
+          if(isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['email'])){
+            $prenom = filter_var($_POST['prenom'], FILTER_SANITIZE_STRING);
+            $nom = filter_var($_POST['nom'], FILTER_SANITIZE_STRING);
+            $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+
+
+            if(is_null(User::where('email', "like", $email)->where('first_name', '!=', $prenom)->where('last_name', '!=', $nom)->first())){
+              $usersession = $_SESSION['user'];
+              $user = User::where('email', "like", $usersession->email)->first();
+              $user->first_name = $prenom;
+              $user->last_name = $nom;
+              $user->email = $email;
+              if($user->save()){
+                $_SESSION['user'] = $user;
+                $this->view['view']->render($response, 'profile.html.twig', array(
+                    'user' => $_SESSION['user'],
+                    'success' => 'Votre profil a été modifié.'
+                ));
+              }else{
+                $this->view['view']->render($response, 'profile.html.twig', array(
+                  'user' => $_SESSION['user'],
+                  'error' => 'Erreur interne. Votre profil n\'a pas été modifié. Contactez un administrateur.'
+                ));
+              }
+            }else{
+              $this->view['view']->render($response, 'profile.html.twig', array(
                 'user' => $_SESSION['user'],
-                'success' => 'Votre profil a été modifié.'
+                'error' => 'Votre nouvelle adresse mail est déjà utilisée'
+              ));
+            }
+          }else{
+            $this->view['view']->render($response, 'profile.html.twig', array(
+              'user' => $_SESSION['user'],
+              'error' => 'Vous devez remplir les champs pour modifier vos informations'
             ));
+          }
 
         }else{
             $this->view['view']->render($response, 'homepage.html.twig', array(
