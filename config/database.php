7fd8ded46b5dfebe07bd320a8cbf1bbc6e8ddf06
Barzimuto <kevinbarre474@gmail.com>
2017-03-30 11:07:21 +0200

    coucou

--- config/database.php
+++ config/database.php
@@ -7,9 +7,9 @@ $capsule = new Capsule;
 $capsule->addConnection([
     'driver'    => 'mysql',
     'host'      => 'localhost',
-    'database'  => 'Videodorant',
+    'database'  => 'videodorant',
     'username'  => 'root',
-    'password'  => 'root',
+    'password'  => '',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
