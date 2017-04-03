cf99c0ae8bf1259c20dc1b0cf098eb3a6c7ffda3
Kixot <seb.kixot@gmail.com>
2017-04-03 16:13:01 +0200

    abonnement sans video

--- app/src/Controllers/CatalogController.php
+++ app/src/Controllers/CatalogController.php
@@ -45,8 +45,10 @@ final class CatalogController extends AbstractController{
         if(count($abonnements) > 0){
             foreach($abonnements as $k=>$v){
                 $video = Video::where('userId', 'like', $v->idUser)->orderBy('dateAjout', 'desc')->first();
-                $video->user = User::where('id', 'like', $video->userId)->first()->username;
-                array_push($videos, $video);
+                if(!is_null($video)){
+                    $video->user = User::where('id', 'like', $video->userId)->first()->username;
+                    array_push($videos, $video);
+                }
             }
         }else
             return $this->getVideosWhenNotLogged();
